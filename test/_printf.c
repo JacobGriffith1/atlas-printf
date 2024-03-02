79bcf4d57b542565faab8bcf34b9075217e78cd1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-02 23:02:55 +0000

    added betty style header to _printf.c

---
+++ test/_printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format designating what should be produced.
+ * Return: N/A
+ */
+
+int _printf(const char *format, ...)
+{
